fff8d368b7c7dc54f4452f6849cc59b6e3e3af3b
Shiqing <shiqing-thu18@yandex.com>
2022-02-21 10:51:54 +0800

    Daily puzzles

--- server.js
+++ server.js
@@ -3,14 +3,31 @@ import { serveFile } from 'https://deno.land/std@0.126.0/http/file_server.ts';
 import { parse as parseYaml } from 'https://deno.land/std@0.126.0/encoding/yaml.ts';
 import { compile as etaCompile, config as etaConfig } from 'https://deno.land/x/eta@v1.12.3/mod.ts';
 
+const epoch = new Date('2022-02-20T16:00:00Z');
+
 const indexHtmlContents = new TextDecoder().decode(await Deno.readFile('page/index.html'));
 const indexTemplate = etaCompile(indexHtmlContents);
+
+const servePuzzle = async (puzzleId) => {
+  const puzzleContents = parseYaml(
+    new TextDecoder().decode(await Deno.readFile(`puzzles/${puzzleId}.yml`))
+  );
+  puzzleContents.id = puzzleId;
+  const pageContents = indexTemplate(puzzleContents, etaConfig);
+  return new Response(pageContents, {
+    status: 200,
+    headers: {
+      'Content-Type': 'text/html; charset=utf-8',
+    },
+  });
+};
 
 const handler = async (req) => {
   const urlMatch = /^(?:https?:\/\/)?[A-Za-z0-9.:-]+(\/.*)?$/g.exec(req.url);
   const reqPath = (urlMatch[1] || '/');
   if (reqPath === '/') {
-    return new Response('under construction');
+    const index = Math.ceil((new Date() - epoch) / 86400000);
+    return servePuzzle(index.toString().padStart(3, '0'));
   }
   if (reqPath.startsWith('/static/')) {
     const fileName = reqPath.substring('/static/'.length);
@@ -20,21 +37,10 @@ const handler = async (req) => {
     const fileName = reqPath.substring('/reveal/'.length);
     return serveFile(req, 'puzzles/reveal/' + fileName);
   }
-
-  // Puzzle
+  // Custom puzzle
   if (reqPath.match(/^\/[A-Za-z0-9]+$/g)) {
     const puzzleId = reqPath.substring(1);
-    const puzzleContents = parseYaml(
-      new TextDecoder().decode(await Deno.readFile(`puzzles/${puzzleId}.yml`))
-    );
-    puzzleContents.id = puzzleId;
-    const pageContents = indexTemplate(puzzleContents, etaConfig);
-    return new Response(pageContents, {
-      status: 200,
-      headers: {
-        'Content-Type': 'text/html; charset=utf-8',
-      },
-    });
+    return servePuzzle(puzzleId);
   }
 };
 
