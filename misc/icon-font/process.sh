d52d8f2f18162940127a5fc354cf3ea4ca3af46e
Shiqing <shiqing-thu18@yandex.com>
2022-02-28 00:08:30 +0800

    Make the remaining few texts translatable

--- misc/icon-font/process.sh
+++ misc/icon-font/process.sh
@@ -3,7 +3,7 @@ pyftsubset rounded-mplus-1c-regular.ttf \
   --unicodes=266a,266d,266f,2714
 pyftsubset fa-solid-900.ttf \
   --output-file=fa-solid-900-subset.ttf \
-  --unicodes=f013,f041,f04b,f04d,f0c8,f0d8,1f7e4,1f4c6,f141,f219,1f319,f55a
+  --unicodes=1f3b5,f013,f041,f04b,f04d,f0c8,f0d8,1f7e4,1f4c6,f141,f219,1f319,f55a
 fontforge -lang=ff -script merge.ff
 woff2_compress icons.ttf
 rm rounded-mplus-1c-regular-subset.ttf fa-solid-900-subset.ttf icons.ttf
