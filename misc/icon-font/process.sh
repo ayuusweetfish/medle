62b35dd269b8b2de42b729bf0842b51038786666
Shiqing <shiqing-thu18@yandex.com>
2022-11-03 22:39:55 +0800

    Use FxEmojis for emoji display
    Squashed commit of the following:
    
    commit b96af8655b5ba8ff015da1c6f9918a9e41768d6a
    Author: Shiqing <shiqing-thu18@yandex.com>
    Date:   Thu Nov 3 22:37:18 2022 +0800
    
        Place emojis in a separate font for WebKit-based browsers to render correctly
    
    commit c935f456dc4bc98f3438279dc2f06d0fe6199d3e
    Author: Shiqing <shiqing-thu18@yandex.com>
    Date:   Thu Nov 3 22:34:11 2022 +0800
    
        Merge FxEmojis

--- misc/icon-font/process.sh
+++ misc/icon-font/process.sh
@@ -4,6 +4,22 @@ pyftsubset rounded-mplus-1c-regular.ttf \
 pyftsubset fa-solid-900.ttf \
   --output-file=fa-solid-900-subset.ttf \
   --unicodes=1f3b5,f013,f041,f04b,f04d,f0c8,f0d8,1f7e4,1f4c6,f141,f219,1f319,f55a,f56b
-fontforge -lang=ff -script merge.ff
+fontforge -lang=ff -script <<EOF
+  Open("fa-solid-900-subset.ttf")
+  em1=\$em
+  Close()
+  Open("rounded-mplus-1c-regular-subset.ttf")
+  ScaleToEm(em1)
+  MergeFonts("fa-solid-900-subset.ttf")
+  Generate("icons.ttf")
+  Close()
+EOF
 woff2_compress icons.ttf
 rm rounded-mplus-1c-regular-subset.ttf fa-solid-900-subset.ttf icons.ttf
+
+pyftsubset FirefoxEmoji.ttf \
+  --output-file=emoji.ttf \
+  --text=`cat ../../page/index.html | perl -CIO -pe 's/[^\p{Emoji}]|[0-9\#]//g'`
+woff2_compress emoji.ttf
+fc-query --format='%{charset}\n' emoji.ttf
+rm emoji.ttf
